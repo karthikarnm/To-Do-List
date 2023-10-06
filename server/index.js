19652d458c8bafa6074e85b6e286053d1473bada
Karthi <85386654+karthikarnm@users.noreply.github.com>
2023-10-06 14:15:42 +0530

    vercel

--- server/index.js
+++ server/index.js
@@ -1,30 +1,37 @@
+// Import necessary modules and packages
+const express = require('express'); // Express.js for creating the server
+const mongoose = require('mongoose'); // Mongoose for MongoDB object modeling
+const dotenv = require('dotenv').config(); // Dotenv for managing environment variables
 
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
+const cors = require('cors'); // Cors for handling Cross-Origin Resource Sharing
 
-const cors = require('cors');
+const routing = require('./routes/ToDoRoutes.js'); // Importing your custom routes from ToDoRoutes.js
 
-const routing = require('./Routes/ToDoRoutes,.js');
-
+// Set the port for the server
 const PORT = 4000;
+
+// MongoDB connection URL (replace with your own credentials)
 const URL = "mongodb+srv://karanamkarthi5:kk@cluster0.gxpfz7s.mongodb.net/To-Do-List"
 
+// Create an instance of the Express application
 const app = express();
 
-
+// Middleware to parse incoming request data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Middleware to handle Cross-Origin Resource Sharing
 app.use(cors());
 
+// Connect to MongoDB
 mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("MongoDB Connected"))
     .catch((err) => console.log(err));
 
-
-
+// Use the custom routes defined in ToDoRoutes.js
 app.use('/', routing);
 
+// Start the server and listen on the specified port
 app.listen(PORT, () => {
     console.log('Listening on port ' + PORT);
 });
--- server/model/ToDoListSchema.js
+++ server/model/ToDoListSchema.js
@@ -1,10 +1,14 @@
+// Import the mongoose library
 const mongoose = require('mongoose');
 
+// Define a Mongoose schema for the ToDoList
 const ToDoListSchema = new mongoose.Schema({
+    // Define a field 'item' with type String
     item: {
         type: String,
-        required: true
+        required: true // The 'item' field is required
     }
-})
+});
 
-module.exports = mongoose.model('ToDoList', ToDoListSchema)
+// Export the Mongoose model for ToDoList using the defined schema
+module.exports = mongoose.model('ToDoList', ToDoListSchema);
